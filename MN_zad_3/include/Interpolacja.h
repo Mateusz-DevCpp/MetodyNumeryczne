5551e1ba0197c526a704431ee86ffc82b66b233b
Mateusz Darul <77622126+Mateusz-DevCpp@users.noreply.github.com>
2021-04-20 15:46:16 +0200

    Zadanie 3 END
    ...

--- MN_zad_3/include/Interpolacja.h
+++ MN_zad_3/include/Interpolacja.h
@@ -10,9 +10,7 @@ class Interpolacja
 {
 public:
     ///Newton
-    static double obliczWartoscFunkcjiInterpolowanej_Newton_wspolczynniki(std::vector<Punkt<double>> wezly, int n);
-    static double obliczWartoscFunkcjiInterpolowanej_Newton_wartosc(std::vector<Punkt<double>> wezly, double x, int n);
-    static double obliczWartoscFunkcjiInterpolowanej_Newton(std::vector<Punkt<double>> wezly, double x, int n);
+    static double obliczWartoscFunkcjiInterpolowanej_Newton(std::vector<Punkt<double>> wezly, double x);
     static std::vector<Punkt<double>> obliczWartosciFunkcjiInterpolowanejNaPrzedziale_Newton(Punkt<double> przedzial, std::vector<Punkt<double>> wezly, double odleglosc_miedy_punktami);
     static double liczAn(std::vector<Punkt<double>> wezly, int n);
     static double liczQn(std::vector<Punkt<double>> wezly, double x, int n);
